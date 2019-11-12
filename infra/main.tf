64528a0846cd4cb4093144404d6f99392e725147
Kelly Stuard <kellystuard@gmail.com>
2019-11-12 22:36:32 +0000

    Refactoring to use variables.

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,20 @@
 resource "azurerm_resource_group" "main" {
   name     = "terraform-azure-example"
-  location = "North Central US"
+  location = var.location
 }
 resource "azurerm_virtual_network" "main" {
   name                = "main-network"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   address_space       = ["10.0.0.0/16"]
+}
+resource "azurerm_lb" "test" {
+  name                = "TestLoadBalancer"
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.test.id}"
+  }
 }
