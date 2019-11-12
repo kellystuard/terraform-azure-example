533c8d71344743067d2062b5a83a54b45ad3e453
Kelly Stuard <kellystuard@gmail.com>
2019-11-12 22:40:48 +0000

    Fixing errors.

--- infra/main.tf
+++ infra/main.tf
@@ -11,5 +11,5 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_lb" "test" {
   name                = "TestLoadBalancer"
   location            = "West US"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
 }
