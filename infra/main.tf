3af91c23ccb61f343db82182b98e677be0b489ae
Kelly Stuard <kellystuard@gmail.com>
2019-11-12 22:38:33 +0000

    Fixing the error.

--- infra/main.tf
+++ infra/main.tf
@@ -12,9 +12,4 @@ resource "azurerm_lb" "test" {
   name                = "TestLoadBalancer"
   location            = "West US"
   resource_group_name = "${azurerm_resource_group.test.name}"
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = "${azurerm_public_ip.test.id}"
-  }
 }
