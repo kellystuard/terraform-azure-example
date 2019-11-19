469d68530df707a3be8f3648cc7c6dc202739f74
Kelly Stuard <kellystuard@gmail.com>
2019-11-19 21:04:32 +0000

    Moving the load-balancer.

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "main" {
 }
 resource "azurerm_lb" "test" {
   name                = "TestLoadBalancer"
-  location            = "West US"
+  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 resource "azurerm_subnet" "internal" {
