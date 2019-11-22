ef3465fc0c9f962fd2963360a1897783edf3a1d5
Kelly Stuard <kellystuard@gmail.com>
2019-11-22 20:30:16 +0000

    Deny RDP; add VM.

--- infra/main.tf
+++ infra/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_network_security_group" "internal" {
     name                       = "testRDP"
     priority                   = 100
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
