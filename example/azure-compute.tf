1acc4e46389b635dabbd476c51032e47e9973bd8
Kelly Stuard <kellystuard@gmail.com>
2021-07-10 17:22:08 -0500

    Can't re-use NIC

--- example/azure-compute.tf
+++ example/azure-compute.tf
@@ -13,8 +13,8 @@ resource "random_password" "password" {
   min_special = 1
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "nic-${var.environment}"
+resource "azurerm_network_interface" "db" {
+  name                = "web-nic-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -52,6 +52,18 @@ resource "azurerm_linux_virtual_machine" "db" {
     application = "example"
     environment = var.environment
     os_type     = "linux"
+  }
+}
+
+resource "azurerm_network_interface" "utility" {
+  name                = "utility-nic-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "example-configuration-${var.environment}"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
