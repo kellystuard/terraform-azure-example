c94fad8052cbbb45ec1d458b23ab7667f67d8689
Kelly Stuard <kellystuard@gmail.com>
2021-07-10 17:39:04 -0500

    Testing rename

--- example/azure-networking.tf
+++ example/azure-networking.tf
@@ -15,8 +15,8 @@ resource "azurerm_subnet" "internal" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-resource "azurerm_network_security_group" "internal" {
-  name                = "InternalSecurityGroup"
+resource "azurerm_network_security_group" "no_rdp" {
+  name                = "NoRDPSecurityGroup"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
