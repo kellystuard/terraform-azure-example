627811d9ad3bae085a68378c3090c2875ba484ba
Kelly Stuard <kellystuard@gmail.com>
2021-07-11 21:05:19 -0500

    Adding delegation to container subnet

--- example/azure-networking.tf
+++ example/azure-networking.tf
@@ -20,6 +20,15 @@ resource "azurerm_subnet" "container" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 resource "azurerm_subnet" "db" {
   name                 = "db-subnet"
