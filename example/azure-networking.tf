db51e44159b2ea36b77e7592d215aceb49a4afca
Kelly Stuard <kellystuard@gmail.com>
2021-07-11 23:57:30 -0500

    Demo: Resource rename

--- example/azure-networking.tf
+++ example/azure-networking.tf
@@ -53,7 +53,7 @@ resource "azurerm_subnet" "db" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 resource "azurerm_network_security_group" "no_rdp" {
-  name                = "NoRDPSecurityGroup"
+  name                = "no-rdp-security-group"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
