97b937007e668e16e1edfae3ba13e8b81bd274c8
kellystuard <kellystuard@gmail.com>
2023-05-31 14:25:27 -0500

    Trying again

--- example2/azure.tf
+++ example2/azure.tf
@@ -26,19 +26,16 @@ resource "azurerm_container_app" "example" {
   template {
     container {
       name   = "examplecontainerapp"
-      image  = "chrch/docker-pets:1.0"
+      image  = "docker.io/chrch/docker-pets:1.0"
       #image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
       cpu    = 0.25
       memory = "0.5Gi"
     }
   }
 
-  registry {
-    server = "docker.io"
-  }
-
   ingress {
     external_enabled = true
+    allow_insecure_connections = false
     target_port      = 5000
     traffic_weight {
       percentage = 100
