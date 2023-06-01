fc97729a6728513b3992f5e2342511d5f2b4f726
kellystuard <kellystuard@gmail.com>
2023-05-31 22:56:58 -0500

    Name = id?

--- example2/azure.tf
+++ example2/azure.tf
@@ -32,7 +32,7 @@ resource "azurerm_container_group" "example" {
 
   diagnostics {
     log_analytics {
-      workspace_id  = azurerm_log_analytics_workspace.example.id
+      workspace_id  = azurerm_log_analytics_workspace.example.name
       workspace_key = azurerm_log_analytics_workspace.example.workspace_id
     }
   }
