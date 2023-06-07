90f664ff9012ebdc690f5e16e785aac62498423f
Kelly Stuard <kellystuard@gmail.com>
2023-06-07 12:09:00 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,11 @@
 ##
 
 locals {
+  ##
+  # This area is used to define the applications and environments. It is created 
+  # at the request of the application owners and updated as they need additional 
+  # environments or metadata about the applicaiton changes.
+  ##
   applications = {
     example1 = {
       cost_center = "88149" # default cost center for all environments
@@ -13,26 +18,15 @@ locals {
         dev = {
           name   = "Example 1 Development"
         }
-        tst = {
-          name   = "Example 1 Test"
-        }
-      }
-    }
-    example2 = {
-      cost_center = "58497" # default cost center for all environments
-      environments = {
-        dev = {
-          name   = "Example 2 Development"
-        }
-        tst = {
-          name   = "Example 2 Special Test" #special cost center for special test environment
-          branch = "test-special"
-          cost_center = "90000"
-        }
       }
     }
   }
 
+  ##
+  # This area is used to define the defaults for applications and environments. If 
+  # not specified at the application or environment level, these become the values 
+  # used. Environment > Application > Environment Default > Application Default
+  ##
   application_defaults = {
     terraform_version = "1.4.6"
     azure_location = "North Central US"
@@ -49,6 +43,10 @@ locals {
     }
   }
 
+  ##
+  # This area is used to merge the applications with defaults and put it in a format 
+  # more easily consumable by Terraform resources.
+  ##
   application_environments = { for ae in flatten([
     for appk, appv in local.applications : [
       # application-environment > application > application-environment defaults > application defaults
